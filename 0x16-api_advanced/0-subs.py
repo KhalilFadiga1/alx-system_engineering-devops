195c148c90fba1d602596295b1ae9b0af10f4b83
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-08-15 20:36:09 +0000

    Updating 0-subs.py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,18 +4,19 @@ Uses Reddit API to print the number of subreddit subscribers
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
-	"""
-	Print the number of subscribers of a a subreddit
-	"""
-	url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-	headers = {"user-agent": "linux: 0x16.advanced: v1 0.0 (by /u/bdov_)"}
-	response = requests.get(url, headers=headers, allow_redirects=False)
-	
-	if response.status_code != 200:
-		return 0
+    """
+    Print the number of subscribers of a a subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"user-agent": "linux: 0x16.advanced: v1 0.0 (by /u/bdov_)"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-	result = response.json().get("data")
-	num_subs = result.get("subscribers")
+    if response.status_code != 200:
+        return 0
 
-	return num_subs
+    result = response.json().get("data")
+    num_subs = result.get("subscribers")
+
+    return num_subs
