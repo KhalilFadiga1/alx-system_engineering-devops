174fdc350b64befbeda69935eaa462073c77e987
KhalilFadiga1 <khalilfadiga667@gmail.com>
2024-08-19 21:22:02 +0000

    Updates

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,13 +10,18 @@ def number_of_subscribers(subreddit):
     Print the number of subscribers of a a subreddit
     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"user-agent": "linux: 0x16.advanced: v1 0.0 (by /u/bdov_)"}
+
+    headers = {
+        "user-agent": "linux: 0x16.advanced: v1 0.0 (by /u/bdov_)"
+    }
+
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return 0
 
     result = response.json().get("data")
+
     num_subs = result.get("subscribers")
 
     return num_subs
